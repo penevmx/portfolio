a2f0f312c326aa3ced62c05006123c68115d23c4
penev <penevmx@gmail.com>
2024-07-06 20:23:28 +0300

    fix: add margin

--- views/Projects/ProjectsListing.tsx
+++ views/Projects/ProjectsListing.tsx
@@ -10,11 +10,11 @@ const ProjectsListing: React.FC<ProjectsType> = () => {
   return (
     <div
       id="Projects"
-      className="flex items-center justify-center flex-col bg-muted"
+      className="flex items-center justify-center flex-col bg-muted "
     >
       <h1 className="text-4xl font-bold mt-10">Projects</h1>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 bg-secondary max-w-[1020px] mt-16 min-w-[360px] animate-fade">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 bg-secondary max-w-[1020px] mt-16 mb-8 min-w-[360px] animate-fade">
         {projectsInfo.map((project) => (
           <ProjectCard
             key={project.title}

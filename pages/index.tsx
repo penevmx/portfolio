0e6cad1084805111d43b81cb2b1d5ed8e2056352
penev <penevmx@gmail.com>
2024-07-06 20:07:41 +0300

    animate divider

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <main>
       <Hero />
-      <Divider />
+      <Divider animated={true} />
       <ProjectsListing />
       <Divider />
       <Contacts />
